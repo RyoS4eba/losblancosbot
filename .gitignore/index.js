85b352ccdd7b727a49a70b418a483ab027d309ec
RyoS4eba <44675007+RyoS4eba@users.noreply.github.com>
2018-11-01 15:44:31 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("*")
+
+bot.on('ready', function() {
+  bot.user.setGame("Command: :help");
+  console.log("Connected");
+});
+
+bot.on('message', message => {
+  if (message.content == prefix + "help"){
+    message.channel.sendMessage("Liste des commandes: \n !création \n !créateur");
+  }
+  
+  if (message.content == prefix + "création"){
+    message.channel("Bot créé le __01/11/2018__ à __15h42__");
+    console.log("Commande effectuée !");
+  }
+  
+  if (message.content == prefix + "créateur"){
+    message.channel("Ryo Saeba#9605");
+    console.log("Commande effectuée !");
+  }
+});
